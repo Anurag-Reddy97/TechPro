d222e1dc9b6428831aa9adb1f24d8e4f5403faac
Anurag <anuragreddyidh@gmail.com>
2020-03-26 16:11:51 +0530

    Assignment updated

---
+++ GitDemo/src/com/nttdata/Demo.java
@@ -0,0 +1,18 @@
+package com.nttdata;
+
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter the  name :");
+   String str =sc.next();
+   System.out.println("enter age");
+   int age=sc.nextInt();
+   System.out.println(str);
+   System.out.println(age);
+	}
+
+}
