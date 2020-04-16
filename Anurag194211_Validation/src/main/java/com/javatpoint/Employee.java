1f620cfd6b78b837df238a57b5dea90981718228
Anurag <anuragreddyidh@gmail.com>
2020-04-16 18:01:01 +0530

    SpringMVC Validation Assessment

---
+++ Anurag194211_Validation/src/main/java/com/javatpoint/Employee.java
@@ -0,0 +1,26 @@
+package com.javatpoint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Employee {
+
+	@NotBlank
+	private String name;
+	@Size(min=1,message="required")
+	private String pass;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+}
---
+++ Anurag194211_Validation/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<html>
+<body>
+<a href="hello">Welcome to Registration Page..!  Click here...</a>
+</body>
+</html>
